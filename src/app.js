e2345d93722df005d0d603f8d99b5bca3df7a64a
saadali <saadali6991550@gmail.com>
2026-02-13 15:19:34 +0500

    add multiple route with next fn

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express=require('express')
 
 const app= express();
-app.use('/test',(req,res)=>{
-    res.send('hello guyz welcome back imran khan')
+app.use('/test',(req,res,next)=>{
+   // res.send(' welcome back imran khan 111')
+    next()
+},(req,res,next)=>{
+   // res.send('welcome imran 222')
+    next()
+},(req,res)=>{
+    res.send('welcome imran 333')
 })
 app.get('/user/:userid',(req,res)=>{
     console.log(req.params)
