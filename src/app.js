dbe23b34cd0002a60ee844241dd60256f5fb831d
saadali <saadali6991550@gmail.com>
2026-02-13 14:20:08 +0500

    dynamic routes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,16 @@ const app= express();
 app.use('/test',(req,res)=>{
     res.send('hello guyz welcome back imran khan')
 })
+app.get('/user/:userid',(req,res)=>{
+    console.log(req.params)
+    res.send({
+        firstName:'saad',
+        lastName:'ali'
+    })
+})
+
+
+
 app.listen(3000,()=>{
     console.log('server is listening on port 3000')
 })
-console.log('hello')
