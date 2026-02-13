3a0506b6b446f03e5abb29a80dccf111800e7f3e
saadali <saadali6991550@gmail.com>
2026-02-13 13:08:37 +0500

    create server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express=require('express')
+
+const app= express();
+app.use('/test',(req,res)=>{
+    res.send('hello guyz welcome back imran khan')
+})
+app.listen(3000,()=>{
+    console.log('server is listening on port 3000')
+})
+console.log('hello')
