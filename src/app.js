6a0ffd77362004d5a7eab084d5e31ceb638bdee2
saadali <saadali6991550@gmail.com>
2026-02-15 19:56:28 +0500

    feed api create

--- src/app.js
+++ src/app.js
@@ -3,6 +3,36 @@ const app= express();
 const ConnectDB=require('./config/database')
 const User=require('./models/user')
 app.use(express.json())
+//get specific user
+app.use('/user',async(req,res)=>{
+    const email= req.body.emailId
+    try{
+   const users=  await User.findOne({emailId:email})
+   if(users.length===0){
+    res.status(404).send('not found user with this  emailid')
+   }else{
+       res.send(users)
+
+
+   }
+ }catch(err){
+    console.log(err.message)
+ }
+})
+//feed will get all user from db just put {}
+app.use('/feed',async(req,res)=>{
+    const email= req.body
+try{const users= await User.find({})
+if(users.length===0){
+    res.status(400).send('not found user')
+}
+else{
+   res.send(users)
+}
+}catch(err){
+console.log('something go wrong')
+}
+})
 
 app.post('/signup',async(req,res)=>{
     console.log(req.body)
