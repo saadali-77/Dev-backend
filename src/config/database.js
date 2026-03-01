0c95a4fbfce90cb9a4ba4e68f2296d68568fdb97
saadali-77 <saadali6991550@gmail.com>
2026-03-01 15:34:08 +0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 const ConnectDB= async()=>{
-await mongoose.connect('mongodb+srv://saadnode:fBd45iI0i968ZWzw@saadnode.bvlplpr.mongodb.net/devtinder')
-
+//await mongoose.connect('mongodb+srv://saadnode:fBd45iI0i968ZWzw@saadnode.bvlplpr.mongodb.net/devtinder')
+await mongoose.connect(process.env.MONGO_URI)
 }
 module.exports= ConnectDB
