ae4ff85842e2f803640aed8e05a58ef5712cfa8b
saadali-77 <saadali6991550@gmail.com>
2026-03-01 13:28:18 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,106 +1,34 @@
-const express=require('express')
-const app= express();
-const cookieparser= require('cookie-parser')
-const ConnectDB=require('./config/database')
-const cors= require('cors')
-//const User=require('./models/user')
- 
- //const jwt=require('jsonwebtoken')
- app.use(cors({
-    origin:'https://dev-frontend-tf7d.vercel.app/',
-    credentials:true
- }))
-app.use(express.json())
-app.use(cookieparser())
-const authRouter= require('./routes/auth')
-const profileRouter= require('./routes/profile')
-const requestRouter= require('./routes/request')
-const userRouter= require('./routes/user')
-app.use('/',authRouter)
-app.use('/',profileRouter)
-app.use('/',requestRouter)
-app.use('/',userRouter)
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const ConnectDB = require('./config/database');
+const cors = require('cors');
 
-//get specific user
-// app.get('/user',async(req,res)=>{
-//     const email= req.body.emailId
-//     try{
-//    const users=  await User.findOne({emailId:email})
-//    if(users.length===0){
-//     res.status(404).send('not found user with this  emailid')
-//    }else{
-//        res.send(users)
+// Connect MongoDB Atlas
+ConnectDB()
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.log('Cannot connect database', err));
 
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'https://dev-frontend-tf7d.vercel.app', // NO trailing slash
+    credentials: true,
+  })
+);
 
-//    }
-//  }catch(err){
-//     console.log(err.message)
-//  }
-// })
-// //delete api to delete user from db
-// app.delete('/user',async(req,res)=>{
-//     const userId=req.body.userId
-//     try{
-//     const user=  await  User.findByIdAndDelete(userId)
-//     res.send('user delete successfully from database')
-//     }catch(err){
-//         console.log(err.message)
-//     }
-// })
-// app.patch('/user/:userId',async(req,res)=>{
-//     const userId=req.params.userId
-//     const data= req.body
-//     try { 
-//     AllowedUpdates=['firstName','lastName','password','age','gender','photoUrl','about','skills','userId']
-//     const isAllowed= Object.keys(data).every((key)=>AllowedUpdates.includes(key))
-//     if(!isAllowed){
-//         throw new Error('you are not allowed to update this field')
-//     }
-//     if(data?.skills?.length>10){
-//         throw new Error('skills should be less than 10')
-//     }
-//     await User.findByIdAndUpdate({_id:userId},data,{
-//     runValidators:true
-// })
-// res.send('user update sucessfully')
-// console.log('user update sucessfully')
-// }catch(err){
-//     console.log(err.message)
-// }
+// Routes with /api prefix for Vercel clarity
+const authRouter = require('./routes/auth');
+const profileRouter = require('./routes/profile');
+const requestRouter = require('./routes/request');
+const userRouter = require('./routes/user');
 
-// })
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/request', requestRouter);
+app.use('/api/user', userRouter);
 
-
-
-// //feed will get all user from db just put {}
-// app.use('/feed',async(req,res)=>{
-//     const email= req.body
-// try{const users= await User.find({})
-// if(users.length===0){
-//     res.status(400).send('not found user')
-// }
-// else{
-//    res.send(users)
-// }
-// }catch(err){
-// console.log('something go wrong')
-// }
-// })
-
-
-
-
-
-
-
-
-ConnectDB().then(()=>{
-    console.log('Database connect now  suceesfully')
-    //app.listen(3000,()=>{
-       // console.log('server is listening on the port 3000')
-    })
-
-
-}).catch(err=>{
-console.log('cannot connect database + err.message')
-})
+// Export app for Vercel serverless
+module.exports = app;
