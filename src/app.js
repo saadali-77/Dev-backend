2f5366e6134f544480902a4f5cde55aa223dc906
saadali-77 <saadali6991550@gmail.com>
2026-07-19 20:25:18 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://devtindersaad.com/',
+    origin: 'https://devtinder-frontend-virid.vercel.app/',
     credentials: true,
   })
 );
