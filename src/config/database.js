b8e5c23e214c671d7ce29074634c14b07a2e75f4
saadali-77 <saadali6991550@gmail.com>
2026-07-19 21:28:18 +0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,32 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose')
+const ConnectDB= async()=>{
+await mongoose.connect('mongodb+srv://saadnode:fBd45iI0i968ZWzw@saadnode.bvlplpr.mongodb.net/devtinder')
 
-const ConnectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Atlas connected successfully!');
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    throw err; // so the server knows the DB didn't connect
-  }
-};
-
-module.exports = ConnectDB;
-
-
-
-
-
-
-
-
-
-
-//const mongoose=require('mongoose')
-//const ConnectDB= async()=>{
-//await mongoose.connect('mongodb+srv://saadnode:fBd45iI0i968ZWzw@saadnode.bvlplpr.mongodb.net/devtinder')
-//await mongoose.connect(process.env.MONGO_URI)
-//}
-//module.exports= ConnectDB
+}
+module.exports= ConnectDB
