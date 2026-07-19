db0adf55c9f23861eb2fdeae4506cb6eae82cdbd
saadali-77 <saadali6991550@gmail.com>
2026-07-19 20:53:06 +0500

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,72 +1,83 @@
-const express=  require('express')
- const authRouter= express.Router()
-  const User= require('../models/user')
-  const {validateSignup}= require('../utils/validation')
-  //const jwt=require('jsonwebtoken')
-  const bcrypt= require('bcrypt')
-  //const { AdminAuth } = require('../middlewares/auth')
+const express = require("express");
+const authRouter = express.Router();
+const User = require("../models/user");
+const { validateSignup } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
-authRouter.post('/signup',async(req,res)=>{
-    try
-    {
-    validateSignup(req)
-    console.log(req.body)
-    const {password,firstName,lastName,emailId,photoUrl,skills,age}= req.body
-    const hashedPassword= await bcrypt.hash(password,10)
-    console.log(hashedPassword)
-const user=new User({firstName,lastName,emailId,password:hashedPassword,photoUrl,skills,age})
-    await user.save()
-res.send('user added or sign sucessfully')
-}
-catch(err){
-res.status(400).send('found an error' + err.message)
-}
-})
-authRouter.post('/login',async(req,res)=>{
-    const {emailId,password}= req.body
-try{
+authRouter.post("/signup", async (req, res) => {
+  try {
+    validateSignup(req);
 
- const user=   await User.findOne({emailId:emailId})
- if(!user){
-    return res.status(404).send('invalid credentials')
- }
-  const ispasswordvalid= await user.validatePassword(password)
-  if(ispasswordvalid){
-    const token= await user.getjwt()
-   // const token= await jwt.sign({_id:user._id},'pnjiiiii',{expiresIn:'1h'})
- console.log(token)
-    res.cookie('token',token,{
-     secure: true,           // ✅ ensures cookie is sent only over HTTPS
-  sameSite: 'None',
-     
-     expires: new Date(Date.now()+3600000)})
-     // res.send('login successfull')
-     res.send(user)
-  } else{
- throw new Error('invalid credentials')
+    const {
+      password,
+      firstName,
+      lastName,
+      emailId,
+      photoUrl,
+      skills,
+      age,
+    } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: hashedPassword,
+      photoUrl,
+      skills,
+      age,
+    });
+
+    await user.save();
+
+    res.status(201).send("User registered successfully");
+  } catch (err) {
+    res.status(400).send(err.message);
   }
-}
-catch(err){
-    res.status(404).send(err.message)
-}})
-authRouter.post('/logout',async(req,res)=>{
-  res.cookie('token',null,
-               
-    {httpOnly: true,
-  secure: true,       // HTTPS only (required for Vercel)
-  sameSite: 'none',   // allows cross-domain
-              
-              expires: new Date(Date.now())})
-  res.send('logout sucessfully')
-  
-})
+});
 
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
+    const user = await User.findOne({ emailId });
 
+    if (!user) {
+      return res.status(401).send("Invalid credentials");
+    }
 
+    const isPasswordValid = await user.validatePassword(password);
 
+    if (!isPasswordValid) {
+      return res.status(401).send("Invalid credentials");
+    }
 
+    const token = await user.getjwt();
 
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,       // Required on Vercel (HTTPS)
+      sameSite: "none",   // Required for frontend & backend on different domains
+      maxAge: 60 * 60 * 1000, // 1 hour
+    });
 
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
- module.exports= authRouter
+authRouter.post("/logout", (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    expires: new Date(0),
+  });
+
+  res.send("Logged out successfully");
+});
+
+module.exports = authRouter;
