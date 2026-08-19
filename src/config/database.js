b50099fb7b5be97d2a797d9c88031d8f5b3898ce
saadali-77 <saadali6991550@gmail.com>
2026-08-19 12:45:07 +0500

    Update MongoDB connection string in database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,8 @@ let isConnected = false;
 const ConnectDB = async () => {
   if (isConnected) return;
 
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(mongodb+srv://saadnode:Rysnqi5O4dPH7wTi@cluster0.jmcv49s.mongodb.net/?appName=Cluster0
+Rysnqi5O4dPH7wTi);
 
   isConnected = true;
   console.log("MongoDB Connected");
